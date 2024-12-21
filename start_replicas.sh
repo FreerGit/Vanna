d27a866d99102991ef4c3c3ff42bd5c392343cc4
freergit <fredrik-98@live.se>
2024-12-21 22:09:23 +0100

    use split pane for wt (start_replicas)

--- start_replicas.sh
+++ start_replicas.sh
@@ -2,6 +2,7 @@
 
 dune build
 
-wt.exe wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 0" 
-wt.exe wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 1" 
-wt.exe wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 2" 
+wt.exe \
+wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 0" \; \
+split-pane wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 1" \; \
+split-pane wsl bash -c "source ~/.profile && cd ~/dev/vanna && ./cli.exe run-replica -addresses 127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002 -replica 2"
